a5f45d80d8c91f429b5d84b3009a7978a1e1db70
pepeajcu <josemiguelajcu@gmail.com>
2024-11-20 21:04:41 -0600

    Setting Up en Desktop

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
 import Layout from "@/components/layout/Layout"
-import Blog1 from "@/components/sections/Blog1"
 import Features12 from "@/components/sections/Features12"
-import Features2 from "@/components/sections/Features2"
-import Features3 from "@/components/sections/Features3"
 import Hero1 from "@/components/sections/Hero1"
-import LogoCloud1 from "@/components/sections/LogoCloud1"
-import Newsletter1 from "@/components/sections/Newsletter1"
-import Pricing1 from "@/components/sections/Pricing1"
 import Services1 from "@/components/sections/Services1"
-import Team1 from "@/components/sections/Team1"
-import Testimonial1 from "@/components/sections/Testimonial1"
 import Static1 from "@/components/sections/Static1"
 export default function Home() {
 
