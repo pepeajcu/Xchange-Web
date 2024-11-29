be821e1c1a8b76c24a6373e9fbf66d6220a63793
Pepe Ajcu <jose.ajcu@grupopublimovil.com>
2024-11-29 08:51:49 -0600

    disable some ESLint rules

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,9 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+    eslint: {
+      ignoreDuringBuilds: true, // Ignora errores de ESLint durante la construcción
+    },
+  };
